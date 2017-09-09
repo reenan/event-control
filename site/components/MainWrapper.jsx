7524a8681bed2fc9eb9b94f6438a467303831dc1
Renan Souza <0renan.souza@gmail.com>
2017-09-09 20:23:01 -0300

    teste site

--- site/components/MainWrapper.jsx
+++ site/components/MainWrapper.jsx
@@ -18,7 +18,7 @@ export default class MainWrapper extends Component {
 			<div>
 				<SideMenu />
 
-				<Route path="/" render={(route) => {
+				<Route path="/event-control" render={(route) => {
 					const { state } = route.location;
 					const iFrameURL = getURL(state.section, state.page);
 
