8ec20d39551c4fb7ef9b53021ce5f006d747227a
Renan Souza <0renan.souza@gmail.com>
2017-11-10 22:50:53 -0200

    fix package

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,4 @@
 const webpack = require("webpack");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const path = require("path");
 
 const dev = process.argv.indexOf('-p') == -1;
