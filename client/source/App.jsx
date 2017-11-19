8feca4e14cfe9b4da4af476ad955705eb6ac9078
Julio Renner <julio.renner@hotmail.com>
2017-11-19 19:34:30 -0200

    [App.jsx] Adicionar rota de login e corrigir path do Menu

--- client/source/App.jsx
+++ client/source/App.jsx
@@ -3,7 +3,7 @@ import { Route } from 'react-router-dom';
 import queryString from "query-string";
 
 
-import Menu from "screens/Partials/Menu/Menu.jsx";
+import Menu from "screens/Home/Partials/Menu/Menu.jsx";
 
 import Modal, { ModalHeader, ModalContent, ModalFooter } from "screens/Partials/Modal/Modal.jsx";
 import { Title, Subtitle } from "screens/Partials/Text/Text.jsx";
@@ -11,7 +11,7 @@ import { TextButton, SquareButton } from "screens/Partials/Button/Button.jsx";
 
 import Login from "screens/Login/Login.jsx";
 
-import EventList from "screens/Partials/EventList/EventList.jsx";
+// import EventList from "screens/Partials/EventList/EventList.jsx";
 
 import 'reset-css/reset.css';
 
@@ -38,54 +38,51 @@ export default class App extends Component {
 
 	render() {
 
-		let fakeList = [
+		// let fakeList = [
 
-			{
-				id: 1,
-				title: "Front In Poa",
-				description: "O sit voluptatem accusantium doloremque laudantium, totam remaperiam, eaque ipsa evento que consagra designers e desenvolvedores da web. Sed ut perspiciatis unde omnis iste natus error sit sit voluptatem accusantium doloremque laudantium, totam remaperiam, eaque ipsa voluptatem accusantium doloremque laudantium, totam remaperiam, eaque ipsa quae ab illo inventore veritatis etaudantium voluptatem lorem ipsum",
-				location: "R. Cel. Genúino, 130 - Centro Histórico, Porto Alegre - RS",
-				date: "17/09/2017 9hr 30min às 19hr 30min",
-				price: "A partir de R$ 250,00",
-				flagged: false,
-				logo: "source/imgs/logo-frontinpoa.png",
-				mainColor: "#e63c82"
-			},
-			{
-				id: 2,
-				title: "BrazilJS",
-				description: "A maior conferência de JavaScript do mundo!",
-				location: "Av. Diário de Notícias, 300 - Cristal, Porto Alegre - RS",
-				date: "17/09/2017 9hr 30min às 19hr 30min",
-				price: "Gratuito",
-				flagged: true,
-				logo: "source/imgs/logo-braziljs.png",
-				mainColor: "#ffde17"
-			},
-			{
-				id: 3,
-				title: "AWS Experience",
-				description: "O principal objetivo principal objetivo principal objetivo do AWS Experience é possibilitar.",
-				location: "Av. Diário de Notícias, 300 - Cristal, Porto Alegre - RS",
-				date: "17/09/2017 9hr 30min às 19hr 30min",
-				price: "R$ 100,00",
-				flagged: false,
-				logo: "source/imgs/logo-aws.png",
-				mainColor: "#f79025"
-			}
-		]
+		// 	{
+		// 		id: 1,
+		// 		title: "Front In Poa",
+		// 		description: "O sit voluptatem accusantium doloremque laudantium, totam remaperiam, eaque ipsa evento que consagra designers e desenvolvedores da web. Sed ut perspiciatis unde omnis iste natus error sit sit voluptatem accusantium doloremque laudantium, totam remaperiam, eaque ipsa voluptatem accusantium doloremque laudantium, totam remaperiam, eaque ipsa quae ab illo inventore veritatis etaudantium voluptatem lorem ipsum",
+		// 		location: "R. Cel. Genúino, 130 - Centro Histórico, Porto Alegre - RS",
+		// 		date: "17/09/2017 9hr 30min às 19hr 30min",
+		// 		price: "A partir de R$ 250,00",
+		// 		flagged: false,
+		// 		logo: "source/imgs/logo-frontinpoa.png",
+		// 		mainColor: "#e63c82"
+		// 	},
+		// 	{
+		// 		id: 2,
+		// 		title: "BrazilJS",
+		// 		description: "A maior conferência de JavaScript do mundo!",
+		// 		location: "Av. Diário de Notícias, 300 - Cristal, Porto Alegre - RS",
+		// 		date: "17/09/2017 9hr 30min às 19hr 30min",
+		// 		price: "Gratuito",
+		// 		flagged: true,
+		// 		logo: "source/imgs/logo-braziljs.png",
+		// 		mainColor: "#ffde17"
+		// 	},
+		// 	{
+		// 		id: 3,
+		// 		title: "AWS Experience",
+		// 		description: "O principal objetivo principal objetivo principal objetivo do AWS Experience é possibilitar.",
+		// 		location: "Av. Diário de Notícias, 300 - Cristal, Porto Alegre - RS",
+		// 		date: "17/09/2017 9hr 30min às 19hr 30min",
+		// 		price: "R$ 100,00",
+		// 		flagged: false,
+		// 		logo: "source/imgs/logo-aws.png",
+		// 		mainColor: "#f79025"
+		// 	}
+		// ]
 
 
 		return (
 			<div className="app">
-				
-				<Route path="/home" component={Menu} />
 
-				<Route path="/" component={Login} />
+				<Route path="/" component={Menu} />
+				<Route path="/login" component={Login} />
 
-
-
-				{/*<EventList list={fakeList} />*/}
+				{/* <EventList list={fakeList} /> */}
 				{/*<Login />*/}
 
 				{/*<p onClick={this.openModal}>abrir</p>
