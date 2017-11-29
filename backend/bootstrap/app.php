c3c9240e5dda3273d63fe2e3abfcae97a9832060
Tcharles Silva <tcharlezin@gmail.com>
2017-11-29 02:23:23 -0200

    Subindo arquivos para passport

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -80,7 +80,11 @@ $app->routeMiddleware([
 
 // $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
+$app->register(Laravel\Passport\PassportServiceProvider::class);
+$app->register(Dusterio\LumenPassport\PassportServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+
+$app->configure('auth');
 
 /*
 |--------------------------------------------------------------------------
